a2120c6743b93aa8c7c1069caf866237b85b7815
alireza.amiri <alireza.amiri@stud.uni-goettingen.de>
2017-08-12 17:14:39 +0200

    Timer Handler is added and running. hello world is removed.
    c u next time! :)

--- app/src/main/java/com/goe_apps/android/timetowork/MainActivity.java
+++ app/src/main/java/com/goe_apps/android/timetowork/MainActivity.java
@@ -1,11 +1,14 @@
 package com.goe_apps.android.timetowork;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,18 +17,34 @@ import android.support.design.widget.NavigationView;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener{
 
+    boolean running;
+    MyTimer timer;
+    int theTime;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        running = false;
+        timer= new MyTimer();
+        theTime = 0;
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if (running) {
+                    theTime = timer.stop();
+                    running = false;
+                    Snackbar.make(view, "Timer is stopped. the Time is: "+ theTime, Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }else{
+                    timer.start();
+                    timer.sendEmptyMessage(0);
+                    running = true;
+                    Snackbar.make(view, "Timer is started.", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
             }
         });
 
@@ -88,5 +107,49 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public static class MyTimer extends Handler
+    {
+
+        public static final int START = 0;
+        public static final int STOP = 1;
+        public static boolean isRunning = false;
+        public static int counter = 0;
+
+        public void start(){
+            isRunning = true;
+        }
+
+        public int stop(){
+            isRunning = false;
+            return counter;
+        }
+
+        @Override
+        public void handleMessage(Message msg)
+        {
+            switch (msg.what)
+            {
+                case START:
+                    // Do something etc.
+                    Log.d("TimerExample", "Timer 1");
+                    if (isRunning) {
+                        counter++;
+                        sendEmptyMessageDelayed(START, 1000);
+                    }else{
+                        sendEmptyMessage(STOP);
+                    }
+                    break;
+                case STOP:
+                    counter = 0;
+                    Log.d("TimerExample", "Timer 2");
+                    break;
+                default:
+                    removeMessages(START);
+                    removeMessages(STOP);
+                    break;
+            }
+        }
     }
 }
